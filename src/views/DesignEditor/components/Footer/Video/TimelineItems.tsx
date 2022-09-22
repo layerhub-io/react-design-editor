fabc571aff46a6fdb9e4b3550c39d25994ad15d5
chloe.bm <chloe@scenify.io>
2022-09-21 23:22:43 -0500

    add context menu for scenes

--- src/views/DesignEditor/components/Footer/Video/TimelineItems.tsx
+++ src/views/DesignEditor/components/Footer/Video/TimelineItems.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { DesignEditorContext } from "~/contexts/DesignEditor"
-import { useEditor, useFrame } from "@layerhub-pro/react"
+import { useEditor, useFrame } from "@layerhub-io/react"
 import { DndContext, closestCenter, PointerSensor, useSensor, DragOverlay } from "@dnd-kit/core"
 import { arrayMove, SortableContext, horizontalListSortingStrategy } from "@dnd-kit/sortable"
 import { restrictToFirstScrollableAncestor, restrictToHorizontalAxis } from "@dnd-kit/modifiers"
 import TimelineItem from "./TimelineItem"
 import { Block } from "baseui/block"
-import { IScene } from "@layerhub-pro/types"
+import { IScene } from "@layerhub-io/types"
 
 export default function () {
   const { currentScene, scenes, currentPreview, setCurrentPreview, setScenes } = React.useContext(DesignEditorContext)
